016082b447ef527c05157325cae5bd112fa97fe0
amal-alqasim14 <560205@student.fontys.nl>
2026-05-25 14:26:48 +0200

    contact content verbeterd

--- showcase-portfolio/src/components/Contact.jsx
+++ showcase-portfolio/src/components/Contact.jsx
@@ -2,15 +2,45 @@ function Contact() {
   return (
     <section id="contact" className="section contact-section">
       <p className="label">Contact</p>
-      <h2>Laten we samenwerken.</h2>
-      <p>
-        Ben je op zoek naar een gemotiveerde stagiair of junior developer? Neem
-        gerust contact met mij op.
-      </p>
 
-      <a href="mailto:jouwemail@example.com" className="primary-btn">
-        Stuur een email
-      </a>
+     <h2>Ik bouw graag mee aan digitale ervaringen.</h2>
+
+      <p className="contact-text">
+        Ik ben een gemotiveerde student/developer met interesse in frontend
+        development, UX/UI en digitale producten.</p> 
+
+        <p className="contact-text">
+        Momenteel ben ik op zoek naar
+        een stageplek waarin ik mijn vaardigheden verder kan ontwikkelen en kan
+        bijdragen aan echte projecten.</p>
+      
+
+      <div className="contact-buttons">
+        <a
+          href="mailto:amal.alqasim2000@gmail.com"
+          className="primary-btn"
+        >
+          Stuur een email
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/amal-alqasim-a83941240/"
+          target="_blank"
+          rel="noreferrer"
+          className="secondary-btn"
+        >
+          LinkedIn
+        </a>
+
+        <a
+          href="https://github.com/amal-alqasim14"
+          target="_blank"
+          rel="noreferrer"
+          className="secondary-btn"
+        >
+          GitHub
+        </a>
+      </div>
     </section>
   );
 }
